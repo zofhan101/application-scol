e48588cd20bd5421d76b8ec6ceafca9743dac281
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2025-01-09 12:08:59 +0300

    fix menu admin data bs target

--- resources/views/app/menu/menu_admin.blade.php
+++ resources/views/app/menu/menu_admin.blade.php
@@ -1,87 +1,78 @@
 <aside id="sidebar" class="sidebar">
+  <ul class="sidebar-nav" id="sidebar-nav">
 
-    <ul class="sidebar-nav" id="sidebar-nav">
+    <li class="nav-item">
+      <a class="nav-link " href="index.html">
+        <i class="bi bi-grid"></i>
+        <span>Tableau de bord</span>
+      </a>
+    </li><!-- End Dashboard Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link " href="index.html">
-          <i class="bi bi-grid"></i>
-          <span>Tableau de bord</span>
-        </a>
-      </li><!-- End Dashboard Nav -->
-      <li>
-        <a class="nav-link inactive" id="refresh-views-link" href="javascript:void(0);">
-            <i class="bi bi-arrow-clockwise" id="grid-icon"></i>
-            <img src="{{ asset('assets/images/loading.gif') }}" alt="Loading..." class="d-none" id="loading-spinner" style="width: 20px; height: 20px;margin-right:2vh;">
-            <i class="bi bi-check-circle text-success d-none" id="success-icon"></i>
-            <span>Mettre à jour la liste des inscrits</span>
-        </a>
-      </li>
-    
+    <li>
+      <a class="nav-link inactive" id="refresh-views-link" href="javascript:void(0);">
+        <i class="bi bi-arrow-clockwise" id="grid-icon"></i>
+        <img src="{{ asset('assets/images/loading.gif') }}" alt="Loading..." class="d-none" id="loading-spinner" style="width: 20px; height: 20px; margin-right: 2vh;">
+        <i class="bi bi-check-circle text-success d-none" id="success-icon"></i>
+        <span>Mettre à jour la liste des inscrits et des matières</span>
+      </a>
+    </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Gestion des utilisateurs</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="{{ route('listeUsers') }}">
-              <i class="bi bi-circle"></i><span>Liste des utilisateurs</span>
-            </a>
-          </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#component-users" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-menu-button-wide"></i><span>Gestion des utilisateurs</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="component-users" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="{{ route('listeUsers') }}">
+            <i class="bi bi-circle"></i><span>Liste des utilisateurs</span>
+          </a>
+        </li>
+      </ul>
+    </li>
 
-        </ul>
-      </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#components-au" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-calendar-plus"></i><span>Année universitaire</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="components-au" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="{{ route('au_form') }}">
+            <i class="bi bi-circle"></i><span>Ouvrir une nouvelle A.U.</span>
+          </a>
+        </li>
+        <li>
+          <a href="{{ route('au_en_cours') }}">
+            <i class="bi bi-circle"></i><span>A.U. en cours</span>
+          </a>
+        </li>
+        <li>
+          <a href="{{ route('create_exam_form') }}">
+            <i class="bi bi-circle"></i><span>Définition examens de l'A.U.</span>
+          </a>
+        </li>
+      </ul>
+    </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-calendar-plus"></i><span>Année universitaire</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="{{ route('au_form') }}">
-              <i class="bi bi-circle"></i><span>Ouvrir une nouvelle A.U.</span>
-            </a>
-          </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#components-ue" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-menu-button-wide"></i><span>Unités d'enseignement</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="components-ue" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="{{ route('crud_ue') }}">
+            <i class="bi bi-circle"></i><span>Définition des U.E. par année</span>
+          </a>
+        </li>
+        <li>
+          <a href="{{ route('codes_barres') }}">
+            <i class="bi bi-circle"></i><span>Codes-barres pour les copies d'examen</span>
+          </a>
+        </li>
+      </ul>
+    </li>
 
-          <li>
-            <a href="{{ route('au_en_cours') }}">
-              <i class="bi bi-circle"></i><span>A.U. en cours</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="{{ route('create_exam_form') }}">
-              <i class="bi bi-circle"></i><span>Définition examens de l'A.U.</span>
-            </a>
-          </li>
-
-        </ul>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Unités d'enseignement</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="{{ route('crud_ue') }}">
-              <i class="bi bi-circle"></i><span>Définition des U.E. par année</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="{{ route('codes_barres') }}">
-              <i class="bi bi-circle"></i><span>Codes-barres pour les copies d'examen</span>
-            </a>
-          </li>
-
-        </ul>
-      </li>
-
-
-    </ul>
-
-  </aside><!-- End Sidebar-->
+  </ul>
+</aside><!-- End Sidebar-->
 
 <script>
   document.addEventListener('DOMContentLoaded', function () {
